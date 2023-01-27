7a2d0657bf379612aba83edfa45f8f86190453d1
Archie Einstein <lukemccullough1701@outlook.com>
2023-01-27 15:30:01 +0000

    updating paths

--- scripts/headerFade.js
+++ scripts/headerFade.js
@@ -1,15 +1,3 @@
-
-
-const addHTML = () => {
-//     document.querySelector('#header').innerHTML = `
-//     <p>hi</p>
-// `;
-    $.get("pages/header.html", function(data) {
-        $("#header").html(data);
-        alert("Ok. Load.");
-    });
-}
-
 // header scrolling effect
 $(window).on('scroll', function(){
     // If the scroll position > 0 the code inside if statement executes
@@ -23,7 +11,3 @@ $(window).on('scroll', function(){
     }
 
 })
-
-window.onload = () => {
-    addHTML();
-}
--- scripts/headerInsert.js
+++ scripts/headerInsert.js
@@ -1,5 +1,5 @@
 const addHTML = () => {
-    $.get("header.html", function(data) {
+    $.get("../pages/header.html", function(data) {
         $("#header").html(data);
         alert("Ok. Load.");
     });
