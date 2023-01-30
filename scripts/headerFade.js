f87d434942cabf866caedb53f45304f5c63ddb37
Archie Einstein <54220356+Athenas0n@users.noreply.github.com>
2023-01-30 01:28:13 +0000

    Update headerFade.js

--- scripts/headerFade.js
+++ scripts/headerFade.js
@@ -1,13 +1,11 @@
 // header scrolling effect
 $(window).on('scroll', function(){
-    // If the scroll position > 0 the code inside if statement executes
     if($(window).scrollTop()){
-        $('header').addClass('hideTopHeader')
-        $('header').removeClass('showTopHeader');
+        $('header').addClass('showTopHeader');
+
     }
     else{
-        $('header').addClass('showTopHeader');
-        $('header').removeClass('hideTopHeader')
-    }
+        $('header').removeClass('showTopHeader');
+    }    }
 
 })
