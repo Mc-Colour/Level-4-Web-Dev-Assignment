a6db1c23b4dfd1ee7cc238ee30b8cd37de06bc12
Archie Einstein <lukemccullough1701@outlook.com>
2023-01-30 22:42:57 +0000

    implemented mobile side header and burger, created drop down header animation and logic

--- scripts/headerFade.js
+++ scripts/headerFade.js
@@ -1,10 +1,51 @@
+// Global Variables
+var transitionTimer;
+
 // header scrolling effect
 $(window).on('scroll', function(){
     if($(window).scrollTop()){
-        $('header').addClass('showTopHeader');
-
+        $('header').addClass('hideHeader');
+        disableSideFade();
+        transitionTimer = setTimeout(disableSideDisplay, 600)
     }
     else{
-        $('header').removeClass('showTopHeader');
+        $("header").removeClass("hideHeader");
     }    
 })
+
+function showSideHeader() {
+    const burger = document.querySelector(".burger");
+    const sideHeader = document.querySelector(".sideHeader");
+    burger.onclick = () => {
+        clearTimeout(transitionTimer);
+        if ((sideHeader.classList.contains("showSideHeader")) || (sideHeader.classList.contains("sideHeaderTransition"))){
+            disableSideFade();
+            transitionTime = setTimeout(disableSideDisplay,600);
+        } else {
+            enableSideDisplay();
+            transitionTimer = setTimeout(enableSideFade);
+        }
+        
+    }
+}
+
+function enableSideFade() {
+    const sideHeader = document.querySelector(".sideHeader");
+    sideHeader.classList.add("sideHeaderTransition");
+}
+function enableSideDisplay() {
+    const sideHeader = document.querySelector(".sideHeader");
+    sideHeader.classList.add("showSideHeader");
+}
+function disableSideFade() {
+    const sideHeader = document.querySelector(".sideHeader");
+    sideHeader.classList.remove("sideHeaderTransition");
+}
+function disableSideDisplay() {
+    const sideHeader = document.querySelector(".sideHeader");
+    sideHeader.classList.remove("showSideHeader");
+}
+
+window.onload = () => {
+    showSideHeader();
+}
