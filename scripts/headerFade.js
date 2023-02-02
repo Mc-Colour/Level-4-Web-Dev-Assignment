3a3281c0db90e0f7e2b9b5dc29a1f73124160c50
Archie Einstein <lukemccullough1701@outlook.com>
2023-02-02 17:22:40 +0000

    debug

--- scripts/headerFade.js
+++ scripts/headerFade.js
@@ -14,7 +14,7 @@ $(window).on('scroll', function(){
 })
 
 function showSideHeader() {
-    const burger = document.getElementById("burgerLogo");
+    const burger = document.querySelector(".burger");
     const sideHeader = document.querySelector(".sideHeader");
     burger.onClick = () => {
       alert("click");
